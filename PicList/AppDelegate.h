57f8d5a7a0d32207ade775b3db97860b17aa1104
Brad Griffith <braddgriffith@gmail.com>
2012-11-07 11:12:46 -0800

    Initial Commit

---
+++ PicList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PicList
+//
+//  Created by Brad Grifffith on 11/7/12.
+//  Copyright (c) 2012 Brad Grifffith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PicList/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PicList
+//
+//  Created by Brad Grifffith on 11/7/12.
+//  Copyright (c) 2012 Brad Grifffith. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
